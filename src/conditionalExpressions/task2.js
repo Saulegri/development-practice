ea3135cb7fa3c889d233c3586305fd8b529a311e
Saule Grisinaite <saulegri@gmail.com>
2022-07-26 16:52:24 +0100

    Fun-27

---
+++ src/conditionalExpressions/task2.js
@@ -0,0 +1,12 @@
+if value(typeof === "boolean"){
+    console.log(`Variable is boolean : ${value}`);
+}
+if value(typeof === "number"){
+    console.log(`Variable is a number. It's square is: ${value}`);
+}
+if value(typeof === "string"){
+    console.log(`Variable is a string:${value}`);
+}
+else {
+    console.log(`Variable type is: ${value}`);
+}
