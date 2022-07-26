4c11955052da1d6913cafea78efb7d2ef4956e23
Saule Grisinaite <saulegri@gmail.com>
2022-07-26 15:34:03 +0100

    FUN-26

---
+++ src/comparisons/task1.js
@@ -0,0 +1,5 @@
+let result = '5' === 5
+console.log(result);
+
+let result = 'ABC' > 'abc'
+console.log(result);
---
+++ src/comparisons/task2.js
@@ -0,0 +1,5 @@
+let a = 4
+console.log(a % 2 == 0)
+
+let a = 5
+console.log(a % 2 == 0)
