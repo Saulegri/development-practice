17e442ad222d7161699ed9c6eb057e4b51a40611
Saule Grisinaite <saulegri@gmail.com>
2022-07-29 15:38:06 +0100

    }else if

--- src/loops/task1.js
+++ src/loops/task1.js
@@ -2,9 +2,7 @@ for (let i = 10; i <= 30; i++) {
 
     if (i % 2 === 0) {
         console.log('even');
-    }
-    else if (i % 2 !== 0) {
+    } else if (i % 2 !== 0) {
         console.log('odd');
     }
-
 }
