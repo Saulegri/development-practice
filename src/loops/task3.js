63c73d900483f020a3c37599970bc1d2b8facbcd
Saule Grisinaite <saulegri@gmail.com>
2022-07-29 14:54:06 +0100

    ,

--- src/loops/task3.js
+++ src/loops/task3.js
@@ -1,10 +1,9 @@
 let course = "Fundamentals";
-count = 0;
+let count = 0;
 
 for (let i = 0; i < course.length; i++) {
     if (course.charAt(i) == 'a') {
         count++;
     }
 }
-
 console.log(count);
