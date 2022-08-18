7ca8c454e4c3284b0f77673c448e242d2e6e3267
Saule Grisinaite <saulegri@gmail.com>
2022-08-18 13:25:11 +0100

    commit

--- src/functionExpressions/task3.js
+++ src/functionExpressions/task3.js
@@ -1,7 +1,9 @@
 const fn = () => () => ('devMentor short function');
-const fn2 = (p) => p();
+const fn2 = (p) => p()
 
 fn2(fn())
+console.log(fn2(fn()))
+
 
 
 
--- src/functionExpressions/task4.js
+++ src/functionExpressions/task4.js
@@ -1,7 +1,2 @@
-// let fn = (...spaces) => (spaces.split(' ').length - 1);
-// fn('dev Mentor', 'devMentor', ' ');
-let fn = (...args) => (
-    let result = 0
-for (let i = 0; i < string.length; i++) {
-    result = args.split
-)
+const fn = (...rest) => console.log(rest.toString().split(' ').length - 1)
+fn('dev Mentor', 'devMentor', ' ');
